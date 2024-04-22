2652fdc22dc85cde1ad103c2d053da83b71cb562
Aroptich <113030792+Aroptich@users.noreply.github.com>
2024-04-22 21:59:11 +0700

    Update urls.py
    Убран слеш с конца пути создание пользователя

--- backend_api/users/urls.py
+++ backend_api/users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import UserCreateView, UserRUDView
 
 urlpatterns = [
-    path('api/register/', UserCreateView.as_view(), name='register'),
+    path('api/register', UserCreateView.as_view(), name='register'),
     path('api/user/<int:pk>/', UserRUDView.as_view(), name='user-detail'),
 ]
